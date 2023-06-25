ca76cca410b41a36e029a9c9a3c0a841da546849
Ronario Cabral <ronariolimacabral@gmail.com>
2023-06-24 23:12:08 -0300

    teste

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
+import GlobalEstyle, { Container } from './stylesGlobal'
+
 import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
 
-import './global.css'
-
 function App() {
   return (
     <>
+      <GlobalEstyle />
       <Header />
-      <Hero />
+      <Container>
+        <Hero />
         <ListaVagas />
-      </div>
+      </Container>
     </>
   )
 }
